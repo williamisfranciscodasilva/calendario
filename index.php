b87c29d9c204e2bf9a8de726f315ee23c0aa316e
williamis <williamisepaula@gmail.com>
2018-04-11 13:31:45 -0300

    alterando o arquivo index.php colando dentro do mesmo estruturahtml e inserindo dois arquivos novos style.css e jquery.js

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
-<meta charset=UTF-8>
-<?php
+<!DOCTYPE HTML>
+<html lang="pt-BR">
+    <head>
+        <meta charset=UTF-8>
+        <title>Calendário de Eventos</title>
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+
+    <body>
+    <?php
     include 'calendario.php';
     /*echo '<pre>';*/
     montaCalendario();
-?>
+    ?>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    </body>
+</html>
